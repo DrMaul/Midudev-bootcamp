f0bbf0fc047f1f2c8526a2ccbd42f91496529b9a
DrMaul <agusfmartinez99@gmail.com>
2022-11-09 21:47:51 -0300

    Ejercicios de React

--- Part1/src/index.js
+++ Part1/src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//FORMA CORTA DE COMPONENTES 
+const Title = ({course}) => <h1>{course}</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// FORMA LARGA
+const Content = (props) => {
+    return (
+      <h3>{props.texto + ": " + props.numero}</h3>
+    )
+}
+
+const Total = (props) => {
+  return (
+    props.a + props.b + props.c
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Title course={course}/>
+      <p>
+        <Content texto={part1} numero={exercises1}/>
+      </p>
+      <p>
+        <Content texto={part2} numero={exercises2}/>
+      </p>
+      <p>
+        <Content texto={part3} numero={exercises3}/>
+      </p>
+      <p>Number of exercises <Total a={exercises1} b={exercises2} c={exercises3}/></p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
